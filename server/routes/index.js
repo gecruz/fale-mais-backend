afdfa39e0bdf9a37ac1691c7040dd6c345237642
Gustavo da Cruz <gecruz.dev@gmail.com>
2017-07-20 12:06:03 -0300

    feat: add initial destinations structure

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,30 @@
 var express = require('express')
 var router = express.Router()
 var Plan = require('../models/plan')
+var Destination = require('../models/destination')
 
 router.get('/', function (req, res, next) {
-  res.send('Hello, World!')
+  res.send('Bem vindo ao Fale Mais!')
 })
 
 // *** api routes *** //
 router.get('/plans', findAllPlans)
+router.get('/destinations', findAllDestinations)
 
 // *** get ALL plans *** //
 function findAllPlans (req, res) {
   Plan.find(function (err, plans) {
+    if (err) {
+      res.json({'ERROR': err})
+    } else {
+      res.json(plans)
+    }
+  })
+}
+
+// *** get ALL destinations *** //
+function findAllDestinations (req, res) {
+  Destination.find(function (err, plans) {
     if (err) {
       res.json({'ERROR': err})
     } else {
--- test/test-server.js
+++ test/test-server.js
@@ -6,6 +6,7 @@ var mongoose = require('mongoose')
 
 var server = require('../server/app')
 var Plan = require('../server/models/plan')
+var Destination = require('../server/models/destination')
 
 var should = chai.should()
 chai.use(chaiHttp)
@@ -39,6 +40,37 @@ describe('Plans', function () {
         res.body[0].should.have.property('minutes')
         res.body[0].name.should.equal('FaleMais 30')
         res.body[0].minutes.should.equal(30)
+        done()
+      })
+  })
+})
+
+describe('Destinations', function () {
+  Destination.collection.drop()
+
+  beforeEach(function (done) {
+    var newDestination = new Destination({
+      origin: '016'
+    })
+    newDestination.save(function (err) {
+      done()
+    })
+  })
+  afterEach(function (done) {
+    Destination.collection.drop()
+    done()
+  })
+
+  it('should list ALL destinations on /destinations GET', function (done) {
+    chai.request(server)
+      .get('/destinations')
+      .end(function (err, res) {
+        res.should.have.status(200)
+        res.should.be.json
+        res.body.should.be.a('array')
+        res.body[0].should.have.property('_id')
+        res.body[0].should.have.property('origin')
+        res.body[0].origin.should.equal('016')
         done()
       })
   })
